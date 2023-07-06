941d5a001fb3ec4f9fc4e6e9fb179ac89c492ad7
ainhoadeendaya <ainhoadeendaya@hotmail.com>
2023-07-06 23:13:17 +0200

    definition of the function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,14 @@
-for num in range(1,101):
-    if num % 3 == 0 and num % 5 == 0:
-        print("fizzbuz")   
-    elif num % 3 == 0:
-        print("fizz")    
-    elif num % 5 == 0:
-        print("buzz")
-    else:
-        print(num)
-num += 1
+def fizz_buzz(max_num):
+
+    for num in range(1,max_num + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            print("fizzbuz")   
+        elif num % 3 == 0:
+            print("fizz")    
+        elif num % 5 == 0:
+            print("buzz")
+        else:
+            print(num)
+    num += 1
+
+fizz_buzz(54)
